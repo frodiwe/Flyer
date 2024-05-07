56a7d5bbf6d62be2ea7d396b71c3e96f0331bea6
Fiodar Navumenka <ggrowling@gmail.com>
2024-05-07 23:47:33 +0200

    settings

--- main.lua
+++ main.lua
@@ -1,18 +1,22 @@
-local MAX_CAMERA_ZOOM = 50;
-local MIN_CAMERA_ZOOM = 15;
+local MAX_CAMERA_ZOOM = 50
+local MIN_CAMERA_ZOOM = 15
 
-local frame = CreateFrame("Frame")
+local Flyer = CreateFrame("Frame")
 local isMountedBefore = IsMounted()
-local unmountedZoom = MIN_CAMERA_ZOOM
-local mountedZoom = MAX_CAMERA_ZOOM
 
-frame:RegisterEvent("PLAYER_MOUNT_DISPLAY_CHANGED")
-frame:SetScript("OnEvent", function(self, event, ...)
-    if event ~= "PLAYER_MOUNT_DISPLAY_CHANGED" then
+Flyer:RegisterEvent("PLAYER_MOUNT_DISPLAY_CHANGED")
+Flyer:RegisterEvent("ADDON_LOADED")
+Flyer:SetScript("OnEvent", function(self,event,...) self[event](self,event,...);end)
+
+function Flyer:PLAYER_MOUNT_DISPLAY_CHANGED(self, event)
+    local isMountedNow = IsMounted()
+    local inInstance, instanceType = IsInInstance()
+
+    if inInstance and not enabledInInstances then
+        print("kek")
         return
     end
-    
-    local isMountedNow = IsMounted()
+    print("lol")
 
     if isMountedNow == isMountedBefore then
         return
@@ -27,4 +31,37 @@ frame:SetScript("OnEvent", function(self, event, ...)
     end
 
     isMountedBefore = isMountedNow
-end)
+end
+
+function Flyer:ADDON_LOADED(self, addon)
+    if not unmountedZoom then
+        unmountedZoom = MIN_CAMERA_ZOOM
+    end
+
+    if not mountedZoom then
+        mountedZoom = MAX_CAMERA_ZOOM
+    end
+
+    if not enabledInInstances then
+        enabledInInstances = true
+    end
+
+    SLASH_FLYER1 = "/flyer"
+end
+
+-- /flyer inst on
+-- /flyer inst off 
+function SlashCmdList.FLYER(msg)
+	msg = string.lower(msg)
+	cmd, state = strsplit(" ", msg, 2)
+
+    if cmd == "inst" then
+        enabledInInstances = (state == "on")
+
+        if enabledInInstances then
+            print("Addon enabled in instances (raids, arena, bg)", enabledInInstances)
+        else
+            print("Addon disabled in instances")
+        end
+    end
+end
